84270d130e5a62ffe2f125cc108e53e313e5e625
khai <khai.buivinh3005@gmail.com>
2023-02-13 12:59:09 +0700

    netflix-homepage

---
+++ components/Header.tsx
@@ -0,0 +1,63 @@
+import { FaBell, FaSearch } from 'react-icons/fa';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  console.log('isScrolled : ', isScrolled);
+
+  useEffect(() => {
+    console.log('scroll');
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      console.log('remove');
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <header className={`${isScrolled && 'bg-red-500'}`}>
+      <div className='flex items-center space-x-2 md:space-x-10'>
+        <img
+          src='https://rb.gy/ulxxee'
+          width={100}
+          height={100}
+          className='cursor-pointer object-contain'
+        />
+
+        <ul className='hidden space-x-4 md:flex'>
+          <li className='headerLink cursor-default font-semibold text-white hover:text-white'>
+            Home
+          </li>
+          <li className='headerLink'>TV Shows</li>
+          <li className='headerLink'>Movies</li>
+          <li className='headerLink'>New & Popular</li>
+          <li className='headerLink'>My List</li>
+        </ul>
+      </div>
+      <div className='flex items-center space-x-4 text-sm font-light'>
+        <FaSearch className='sm hidden h-6 w-6 sm:inline' />
+
+        <p className='hidden lg:inline'>Kids</p>
+        <FaBell className='sm hidden h-6 w-6 sm:inline' />
+
+        <Link href='/account'>
+          <img
+            src='https://rb.gy/g1pwyx'
+            alt=''
+            className='cursor-pointer rounded'
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
